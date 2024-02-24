f364cc2cd54b121ee842f11a55c6bb1218d7c18e
LutekDev <lutekdev@gmail.com>
2024-02-23 21:42:49 -0300

    📦 NEW: Added New Files in 23/02/24

--- lua.AULAS/FernandoAmaral.AULAS/aula.003/main.lua
+++ lua.AULAS/FernandoAmaral.AULAS/aula.003/main.lua
@@ -1,2 +1,18 @@
 -- Declarando Váriveis
-print("Hello, World")
+-- print("Hello, World")
+
+-- Criando Váriaveis
+local energia = 3
+local nome = "LutekDev"
+local passouFase = false
+
+-- Somando Valores
+local maisenergia = 4
+-- print(energia + maisenergia)
+
+-- Concatenando Texto
+print("Nome do Jogador: " .. nome)
+
+print(type(nome))
+print(type(energia))
+print(type(passouFase))
